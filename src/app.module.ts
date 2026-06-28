b00bd79a92e9ed080563a0b17388b44545eb2ef3
jbourguignon1 <john.bourguignon2003@gmail.com>
2026-06-28 22:40:43 +0200

    Link database to API Part2 and configuring configModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { UsersController } from './users/controller/users.controller';
 import { UsersModule } from './users/users.module';
 import {ConfigService, ConfigModule} from '@nestjs/config';
 import {TypeOrmModule } from '@nestjs/typeorm';
-import { MyConfigModule } from './config/config.module';
+import { DbConfigModule } from './config/config.module';
 
 @Module(
   {
@@ -13,22 +13,22 @@ import { MyConfigModule } from './config/config.module';
               TypeOrmModule.forRootAsync({
                 //forRootAsync is usefull to make module options asynchrone and to write data ine the .env
                 //configModule helps to charge ans to import all the variable from .env
-                imports: [MyConfigModule],
+                imports: [DbConfigModule],
                 //useFactory make function to config TypeORM when NestJS need it, despistes on object with forRoot
                 useFactory: (configService: ConfigService) => ({
                     type: 'postgres',
-                    host: configService.get('DATABASE_HOST'),
-                    port: +configService.get('DATABASE_PORT'),
+                    host: configService.get('database.host'),
+                    port: +configService.get('database.port'),
                     //+ useful to make port in number like (Number)(configService.get('port'))
-                    username: configService.get('DATABASE_USERNAME'),
-                    password: configService.get('DATABASE_PASSWORD'),
-                    database: configService.get('DATABASE_NAME'),
+                    username: configService.get('database.username'),
+                    password: configService.get('database.password'),
+                    database: configService.get('database.name'),
                     entities: [],
-                    synchronize: false,
+                    synchronize: false,    
                   }),
 
               inject: [ConfigService],
-              }), MyConfigModule
+              })
             ],
   controllers: [AppController, UsersController],
   providers: [AppService],
--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,6 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import configurations from './configurations';
 
 @Module({
-
+   imports : [
+      ConfigModule.forRoot({
+         load: [configurations],
+         envFilePath: '.env',
+         isGlobal: true // means it's a global module
+      })
+   ]
 })
-export class MyConfigModule {}
+export class DbConfigModule {}
